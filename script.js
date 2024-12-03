b1c50f047a36893c8c6a55083a3f7930d22871d0
Richardson <ricardoelias556@gmail.com>
2024-12-02 23:06:39 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+if ('serviceworker' in navigator) {
+    window.addEventListener('load',function(){
+        navigator.serviceWorker.register('./serviceworker.js')
+        .then(registro => {
+            console.log("El service worker se registro de manera correcta");
+        })
+        .catch(e =>{
+            console.warn("El service worker no se registro",e);
+        })
+    })
+}
---
+++ serviceworker.js
@@ -0,0 +1,63 @@
+
+const CACHE_NAME = "V1_cache_PWA_IEVND";
+
+urlsToCache = [
+    './',
+    './assets/cine.jpg',
+    './assets/cv.jpg',
+    './assets/dark.jpg',
+    './assets/riego.jpg',
+    '/assets/fut.jpg',
+    '/assets/fut2.jpg',
+    '/assets/futbol.png',
+    '/assets/nivel1.png',
+    './css/styles.css'
+];
+
+//Funcion de instalacion
+//almacena el nombre y los archivos que van a ir guardados en cache
+
+self.addEventListener('install', e =>{
+    e.waitUntil( //le decimos que detenga el evento hasta que se ejecute lo siguiente
+        caches.open(CACHE_NAME)
+        .then(cache =>{
+            return cache.addAll(urlsToCache)
+            .then(() => self.skipWaiting)
+        })
+
+    )
+})
+
+self.addEventListener('activate', e =>{
+    const listaBlancaCache = [CACHE_NAME];
+
+    e.waitUntil(
+        caches.keys()
+        .then(nombresCache => {
+            return Promise.all(
+                nombresCache.map(nombresCache =>{
+                    if(listaBlancaCache.indexOf(nombresCache) === -1){
+                        return caches.delete(nombresCache)
+                    }
+                })
+            )
+        })
+        //activamos la cache actualizada
+        .then(()=> self.clients.claim())
+    )
+
+})
+
+
+self.addEventListener('fetch', e =>{
+    e.respondWith(
+        caches.match(e.request)
+        .then(res =>{
+            if(res)
+            {
+                return res
+            }
+            return fetch(e.request)
+        })
+    )
+})
